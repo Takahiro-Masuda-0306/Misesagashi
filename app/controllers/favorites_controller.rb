ce38b136ebc3eb5c9be46eb81df531fedcf0f491
masuda takahiro <taka.baseball.0306@gmail.com>
2020-04-03 09:21:13 +0000

    modify favorite function -version1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,22 +5,25 @@ class FavoritesController < ApplicationController
     restaurant = Restaurant.find(params[:restaurant_id])
     current_user.approve(restaurant)
     flash[:success] = 'お気に入りに追加しました'
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
     restaurant = Restaurant.find(params[:restaurant_id])
-    favorite = Favorite.find_by(restaurant_id: restaurant.id)
-    user = User.find_by(id: favorite.user_id)
+    favorites = Favorite.where(restaurant_id: restaurant.id)
     
-    unless current_user == user
-      flash[:danger] = '存在しないお気に入りです'
+    favorites.each do |favorite|
+      @user = User.find_by(id: favorite.user_id)
+    end
+    
+    if current_user == @user  
+      current_user.unapprove(restaurant)
+      flash[:danger] = 'お気に入りから削除しました'
+      redirect_to approvings_user_path(current_user)
+    else
+      flash[:danger] = '不正なアクセスです'
       redirect_to root_url
     end
-      
-    current_user.unapprove(restaurant)
-    flash[:danger] = 'お気に入りから削除しました'
-    redirect_back(fallback_location: root_path)
   end
   
 end
